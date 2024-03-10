6cd56049d6a91b1a7e153786efa374ae9db1b12e
Arthur <79015591+arthur-adriansens@users.noreply.github.com>
2024-03-10 09:47:36 +0100

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,19 @@
+const server = Bun.serve({
+  fetch(req) {
+    const path = new URL(req.url).pathname;
+    console.log(path)
+
+    if (path == "/check_status") return new Response("Connected!");
+
+    return new Response("Page not found :(", { status: 404 });
+  },
+  error(error) {
+    return new Response(`<pre>${error}\n${error.stack}</pre>`, {
+      headers: {
+        "Content-Type": "text/html",
+      },
+    });
+  },
+});
+
+console.log(`Listening on localhost:${server.port}`);
