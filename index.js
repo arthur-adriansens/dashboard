a0d0dcca05fc5247d7ef864ef05e137b5c737051
Arthur <79015591+arthur-adriansens@users.noreply.github.com>
2024-03-10 12:08:38 +0100

    enabled cors (cross site acces)

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
+/** @format */
+
 import express from "express";
+import cors from "cors";
 const app = express();
 const port = 3000;
 
+app.use(cors());
+
 app.get("/", (req, res) => {
-	res.send("Hello World!");
+    res.send("Hello World!");
 });
 
 app.get("/check_status", (req, res) => {
-	res.send("connected");
+    res.send("connected");
 });
 
 app.listen(port, () => {
-	console.log(`Listening on port ${port}`);
+    console.log(`Listening on port ${port}`);
 });
